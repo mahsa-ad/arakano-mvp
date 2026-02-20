57ba77075e6ca9f3da74206af31153184ee20235
FaezehZRN <farahani.faezeh98@gmail.com>
2026-02-20 20:45:55 +0330

    apply css to spaceTabs.jsx

--- src/components/SpaceTabs.tsx
+++ src/components/SpaceTabs.tsx
@@ -5,19 +5,20 @@ import "./SpaceTabs.css"
 type Props = {
   value: string;
   onChange: (id: string) => void;
-  items: { id: string; label: string }[];
+  items: { id: string; label: string; image: string }[];
 };
 
 export default function SpaceTabs({ value, onChange, items }: Props) {
   return (
-    <div style={{ display: "flex", justifyContent: "center", gap: 16 }}>
+    <div className="spaceTabs-container">
       {items.map((item) => (
         <div
           key={item.id}
           onClick={() => onChange(item.id)}
           className="space-card">
-            <img src="" alt="" />
-          {item.label}
+            <img src={item.image} alt={item.label} />
+            <span>{item.label}</span>
+          
         </div>
       ))}
     </div>
